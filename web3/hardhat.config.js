aed0bef9bb183e8da26eeca7c463bc46f182f493
jonas-sebera <sebejaz99@gmail.com>
2023-10-14 10:36:26 +0200

    Hardhat config

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://rpc.ankr.com/eth_sepolia",
+        acounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
