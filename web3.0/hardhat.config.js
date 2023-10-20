beb2d17e83c65692b2ec2a957dd878b2802e3653
jonas-sebera <sebejaz99@gmail.com>
2023-10-20 08:08:54 +0200

    Debug update

---
+++ web3.0/hardhat.config.js
@@ -0,0 +1,21 @@
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.9",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://rpc.ankr.com/eth_sepolia",
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
