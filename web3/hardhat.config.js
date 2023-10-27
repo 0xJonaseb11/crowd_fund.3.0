aab389bffce33e84b7ffa4424e3f6654fa57063c
jonas-sebera <sebejaz99@gmail.com>
2023-10-27 07:33:40 +0200

    init debug

---
+++ web3/hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://rpc.ankr.com/eth_sepolia",
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
