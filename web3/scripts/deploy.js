3cb5be9e5fa50350ad2acba8ade9ae4e6f3f3bbd
jonas-sebera <sebejaz99@gmail.com>
2023-10-27 12:19:18 +0200

    update randomness

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -30,4 +30,16 @@ async function main() {
   console.log("Deploying Random House Assignment Contract...");
 
   const RandomHouse = await hre.ethers.getContractFactory("RandomHouseAssignment");
+  const randomHouse = await RandomHouse.deploy(hogwartsAddress, vrfCoordinatorV2Address, subId, keyHash, callbackGasLimit);\
+
+  //waiting for confirmation blocks(again)
+  while(currentBlock + 5 (await hre.ethers.provider.getBlockNumber()));
+
+  //getting the contract address(again)
+  const randomAddress = await randomHouse.getAddress();
+  console.log("Random House Assignment deployed to: ", randomAddress);
+
+  //  Transfer ownership
+  
+
 }
