56bf9cc2555324338a069f955d879012e04651ef
jonas-sebera <sebejaz99@gmail.com>
2023-10-28 05:22:22 +0200

    updated tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js, jsx, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"],
+      },
+      boxShadow: {
+        secondary: "10px 10px 20px rgba(2, 2, 0.25)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
