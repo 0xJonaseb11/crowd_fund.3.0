4f777cc91b2a4c31003a05c699fce56a8917bdbd
jonas-sebera <sebejaz99@gmail.com>
2023-12-04 06:34:08 +0200

    update navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,6 +33,19 @@ const Navbar = () => {
               else 'connect()'
             }}
             />
+
+            <Link to='/profile'>
+              <div className='w-[52px] h-[40px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer' >
+                <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain'/>
+              </div>
+            </Link>
+            </div>
+
+            {/* Small screen navigation */}
+            <div className='sm:hidden flex justify-between items-center relative'>
+            <div className='w-[52px] h-[40px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer' >
+                <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain'/>
+              </div>
             </div>
           </div>
 
