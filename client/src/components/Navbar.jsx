97fffcbddf24e924b1ebb3c125e4b98e94576d87
jonas-sebera <sebejaz99@gmail.com>
2023-12-04 06:07:02 +0200

    update navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,12 +16,29 @@ const Navbar = () => {
   return (
     <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
       <div className='lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]'>
-        
-      </div>
-    </div>
-  )
+       
+       <input type="text" placeholder='Search for campaigns' className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4acd8d] text-white bg-transparent outline-none'/>
+        <div className='w-[72px] h-full rounded-[20px] bg-[#4acd8d] justify-center cursor-pointer'>
+          <img src={search} alt="search" className='w-[15px] h-[15px] object-contain' />
+          </div> 
+          </div>
+
+          <div className='sm:flex hidden flex-row justify-end gap-4'>
+            <CustomButton 
+            btnType='button'
+            title={address ? 'Create a campaign' : "Connect Wallet"}
+            styles={address ? "bg-[#1dc071]" : 'bg-[#8c6dfd]'}
+            handleClick={() => {
+              if (address) {
+
+              }
+            }}
+            />
+            </div>
+          </div>
+
+    
+  );
 }
 
-export default Navbar
-
-export default Navbar
+export default Navbar;
