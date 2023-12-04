8e9b1c33ac4cae0471ac314c7c82a29d1081c33c
jonas-sebera <sebejaz99@gmail.com>
2023-12-04 06:26:15 +0200

    custombutton init

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function CustomButton() {
+const CustomButton = ( {btnType, title, handleClick, styles}) => {
+
   return (
-    <div>CustomButton</div>
+    <button 
+    type={btnType}
+    className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+    onClick={handleClick} 
+    >
+        {title}
+    </button>
   )
 }
 
