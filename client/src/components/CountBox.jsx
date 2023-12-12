9493961a4b700fa92267033355818a37602783a0
jonas-sebera <sebejaz99@gmail.com>
2023-12-12 08:39:14 +0200

    update

--- client/src/components/CountBox.jsx
+++ client/src/components/CountBox.jsx
@@ -2,10 +2,4 @@ import React from 'react'
 
 const CountBox = ({title, value}) => {
   return (
-    <div className='flex flex-col items-center w-['>
-
-    </div>
-  )
-}
-
-export default CountBox
+ult CountBox
