30243118bda15702d0183de2ac64beafe2ed9dc2
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-27 22:28:18 +0200

    update fix typo

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -22,7 +22,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
 
         {!isLoading && campaigns.length === 0 && (
           <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
-            You have not created any campigns yet
+            You have not created any campaigns yet
           </p>
         )}
 
